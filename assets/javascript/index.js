6ed7a3c01caebc3c7ffc7977828048703e7a9eb4
nabiya <m.nabiya@gmail.com>
2018-05-16 15:14:00 -0500

    Changes to skills section

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('a[href^="#"]').on("click", function(event) {
 		var target = $(this.getAttribute("href"));
 		if (target.length) {
@@ -13,4 +13,25 @@ $(document).ready(function() {
 				);
 		}
 	});
+});
+*/
+var scroll_pos = 0;
+var scroll_time;
+ 
+$(window).scroll(function() {
+    clearTimeout(scroll_time);
+    var current_scroll = $(window).scrollTop();
+    
+    if (current_scroll >= $('.navbar').outerHeight()) {
+        if (current_scroll <= scroll_pos) {
+            $('.navbar').removeClass('hidden');    
+        }
+        else {
+            $('.navbar').addClass('hidden');  
+        }
+    }
+        
+    scroll_time = setTimeout(function() {
+        scroll_pos = $(window).scrollTop();
+    }, 100);
 });
