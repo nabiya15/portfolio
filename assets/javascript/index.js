d89a576fa688892e5b3eb55a8bbfcf6a1a25c1c6
unknown <m.nabiya@gmail.com>
2020-06-10 04:43:39 -0500

    updates

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,37 +1,11 @@
-/*$(document).ready(function() {
-	$('a[href^="#"]').on("click", function(event) {
-		var target = $(this.getAttribute("href"));
-		if (target.length) {
-			event.preventDefault();
-			$("html, body")
-				.stop()
-				.animate(
-					{
-						scrollTop: target.offset().top
-					},
-					1000
-				);
-		}
-	});
-});
-*/
-var scroll_pos = 0;
-var scroll_time;
- 
-$(window).scroll(function() {
-    clearTimeout(scroll_time);
-    var current_scroll = $(window).scrollTop();
-    
-    if (current_scroll >= $('.navbar').outerHeight()) {
-        if (current_scroll <= scroll_pos) {
-            $('.navbar').removeClass('hidden');    
-        }
-        else {
-            $('.navbar').addClass('hidden');  
-        }
-    }
-        
-    scroll_time = setTimeout(function() {
-        scroll_pos = $(window).scrollTop();
-    }, 100);
-});
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-5em";
+  }
+  prevScrollpos = currentScrollPos;
+}
